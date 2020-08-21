adc479f44c9231ee5ae9085448f967415dab1961
R Sánchez <rene.as.himself@gmail.com>
2020-08-21 18:14:05 -0500

    Add card button

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-interface Props {
+interface CardProps {
   text: string
 }
 
-const Card = ({ text }: React.PropsWithChildren<Props>) => {
+const Card = ({ text }: React.PropsWithChildren<CardProps>) => {
   return (
     <div className='w-full mb-2 py-2 px-4 bg-gray-100 rounded-sm shadow-md cursor-pointer'>
       {text}
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import AddNewItem from './AddNewItem'
 
-interface Props {
+interface ColumnProps {
   text: string
 }
 
-const Column = ({ text, children }: React.PropsWithChildren<Props>) => {
+const Column = ({ text, children }: React.PropsWithChildren<ColumnProps>) => {
   return (
     <div className='flex-grow-0 w-64 w-96 mr-6 p-2 bg-gray-300 rounded shadow'>
       <h1 className='pt-2 pb-3 px-4 font-bold'>{text}</h1>
       {children}
+
+      <AddNewItem toggleButtonText='+ Add another task' onAdd={console.log} dark />
     </div>
   )
 }
